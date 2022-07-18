a844ab99ddc21a3c03c9245907af2c1742fca0b4
["JOY", "MirrorVlogger"] <fajendagbajoy@gmail.com>
2022-07-18 09:58:48 +0100

    PROJ1-6 project_structure

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,204 @@
+<?php
+    use \App\Http\Controllers\HelperFunctionsController;
+?>
+
+<div class="container-fluid navbar-dark" style="background: #ffffff; color: black;">
+    {{-- @auth
+        @forelse (Auth::user()->notifications as $notification)
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    @switch($notification->data['type'])
+                        @case('comment')
+                            <a href="{{$notification->data['url']}}">{{$notification->data['commentedUsername']}} commented on your post: {{$notification->data['title']}}</a>
+                            <span class="badge badge-primary badge-pill" style="background: #e29fee">{{$notification->data['commentedUsername']}}</span>
+                            @break
+                        @case('audio')
+                            <a href="{{$notification->data['url']}}">{{$notification->data['commentedUsername']}} commented with voice note on your post: {{$notification->data['title']}}</a>
+                            <span class="badge badge-primary badge-pill" style="background: #e29fee">{{$notification->data['commentedUsername']}}</span>
+                            @break
+                        @case('reply')
+                            <a href="{{$notification->data['url']}}">{{$notification->data['commentedUsername']}} replied your comment: {{$notification->data['title']}}</a>
+                            <span class="badge badge-primary badge-pill" style="background: #e29fee">{{$notification->data['commentedUsername']}}</span>
+                            @break
+                    @endswitch
+                </li>
+            </ul>
+        @empty
+            <strong>U do not have any notification yet</strong>
+        @endforelse
+    @endauth --}}
+
+    <header class="blog-header py-1">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="col-2 pt-1">
+                <a class="navbar-brand link-mirror text-dark" href="{{ url('/') }}">
+                    <!-- {{ config('app.name', 'MirorLog') }} -->
+                    <img src="{{ asset('/storage/sounds/mirror2.png') }}">
+                </a>
+            </div>
+            <div class="col-8">
+                {{--
+                    <a class="blog-header-logo link-mirror nav-link text-dark" id="post_content_nav" href="#">POST</a>
+
+                    <input type="text" id="search" name="search" placeholder="Search for contents"
+                        class="form-control form-control-md @error('password') border border-danger @enderror" style="background: white;"/>
+                    <div id="search_result" class="col-md-8" style="position: absolute; z-index:1;"></div>
+
+                    <button type="button" class="btn btn-outline-mirror" id="show_add_contents"
+                            data-bs-toggle="modal" data-bs-target="#content">
+                        <i style="font-size:15px">✍🏽</i> Content</button>
+                    @include('content.add-content.content')
+                --}}
+                <div class="TopHeader">
+                    <div class="InputBox">
+                        <input type="search" placeholder="Search MirrorLog" id="SearchInput">
+                        <div class="SearchIcon">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </div>
+                        <ul class="SearchResult" id="SearchResult"></ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-2 d-flex justify-content-end align-items-center">
+                @auth
+                    <li class="navbar-nav nav-item dropdown">
+                        <a href="#" id="UserNameDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            @if (Storage::exists("public/profile/$logged_in_user->username/profile_location/$logged_in_user->profile_location"))
+                                {{-- <img src="/storage/profile/{{Auth::user()->username}}/profile_location/{{Auth::user()->profile_location}}"
+                                        alt="{{ Auth::user()->username }} - MirrorLog" class="user__photo"
+                                        style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem"> --}}
+                                <div class="container" style="position: relative">
+                                    <img src="/storage/profile/{{Auth::user()->username}}/profile_location/{{Auth::user()->profile_location}}"
+                                            alt="{{ Auth::user()->username }} - MirrorLog" class="user__photo"
+                                            style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem">
+                                    @if (Auth::user()->unreadNotifications->count())
+                                        <span class="badge" style="background:#3d1540; position:absolute;
+                                                top: -2px; left: 36px; font-size:10px; border-radius: 50%">
+                                            {{ Auth::user()->unreadNotifications->count() <=9  ? Auth::user()->unreadNotifications->count() : '9+' }}
+                                        </span>
+                                    @endif
+                                </div>
+                            @else
+                                @if (isset(Auth::user()->profile_pix) && !empty(Auth::user()->profile_pix))
+                                    <div class="container" style="position: relative">
+                                        <img src="{{ Auth::user()->profile_pix }}"
+                                            alt="{{ Auth::user()->username }} - MirrorLog" class="user__photo"
+                                            style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem">
+                                        @if (Auth::user()->unreadNotifications->count())
+                                            <span class="badge" style="background:#3d1540; position:absolute;
+                                                    top: -2px; left: 36px; font-size:10px; border-radius: 50%">
+                                                {{ Auth::user()->unreadNotifications->count() <=9  ? Auth::user()->unreadNotifications->count() : '9+' }}
+                                            </span>
+                                        @endif
+                                    </div>
+                                @else
+                                    <div class="container" style="position: relative">
+                                        <img src="{{ asset('images/default_user picture icon.png') }}"
+                                            alt="MirrorLog default profile pix" class="user__photo"
+                                            style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem">
+                                        @if (Auth::user()->unreadNotifications->count())
+                                            <span class="badge" style="background:#3d1540; position:absolute;
+                                                    top: -2px; left: 36px; font-size:10px; border-radius: 50%">
+                                                {{ Auth::user()->unreadNotifications->count() <=9  ? Auth::user()->unreadNotifications->count() : '9+' }}
+                                            </span>
+                                        @endif
+                                    </div>
+                                @endif
+                            @endif
+                        </a>
+
+                        <ul class="dropdown-menu" aria-labelledby="UserNameDropdown">
+                            <li>
+                                <a class="dropdown-item" href="{{ route('viewprofile', $logged_in_user) }}">
+                                    @if (Storage::exists("public/profile/$logged_in_user->username/profile_location/$logged_in_user->profile_location"))
+                                        <img src="/storage/profile/{{Auth::user()->username}}/profile_location/{{Auth::user()->profile_location}}"
+                                                alt="{{ Auth::user()->username }} - MirrorLog" class="user__photo"
+                                                style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem">
+                                    @else
+                                        @if (isset(Auth::user()->profile_pix) && !empty(Auth::user()->profile_pix))
+                                            <img src="{{ Auth::user()->profile_pix }}"
+                                                alt="{{ Auth::user()->username }} - MirrorLog" class="user__photo"
+                                                style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem">
+                                        @else
+                                            <img src="{{ asset('images/default_user picture icon.png') }}"
+                                                alt="MirrorLog default profile pix" class="user__photo"
+                                                style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem">
+                                        @endif
+                                    @endif
+                                    {{ HelperFunctionsController::text_resize(auth()->user()->username, 18,'') }}
+                                </a>
+                            </li>
+                            @if (Auth::user()->unreadNotifications->count())
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('notification') }}"><i class="fa fa-solid fa-bell"></i>
+                                        {{ Auth::user()->unreadNotifications->count() <=9  ? Auth::user()->unreadNotifications->count() : '9+' }} Notification
+                                    </a>
+                                </li>
+                            @else
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('notification') }}"><i class="fa fa-solid fa-bell"></i> 0 Notification</a>
+                                </li>
+                            @endif
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"><i class="fa fa-solid fa-door-open"></i> Logout</a>
+                            </li>
+                            <li class="dropdown-divider"></li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('about') }}"><i class="fa fa-solid fa-book"></i> About MirrorLog</a>
+                            </li>
+                        </ul>
+                    </li>
+                @endauth
+
+                @guest
+                    <li class="navbar-nav nav-item dropdown">
+                        <a href="#" id="LoginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <!-- <a class="link-mirror text-dark nav-link dropdown-toggle" href="" id="LoginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> -->
+                            <!-- My Account -->
+                            <img src="{{ asset('images/default_user picture icon.png') }}"
+                                    alt="MirrorLog default profile pix" class="user__photo"
+                                    style="border-radius: 50%; height: 1.5rem; width: 1.5rem; margin-right: 0.2rem">
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="LoginDropdown">
+                            <li>
+                                <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('register') }}">Register</a>
+                            </li>
+                            <li class="dropdown-divider"></li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('about') }}"><i class="fa fa-solid fa-book"></i> About MirrorLog</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                @endguest
+
+            </div>
+        </div>
+    </header>
+
+</div>
+
+{{-- <div class="container">
+
+    <div class="nav-scroller py-1 mb-2">
+        <nav class="nav justify-content-between">
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">World</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">U.S.</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Technology</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Design</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Culture</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Business</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Politics</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Opinion</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Science</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Health</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Style</a>
+            <a class="btn btn-sm btn-outline-mirror p-1" href="#">Travel</a>
+        </nav>
+    </div>
+
+</div> --}}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <meta name="google-site-verification" content="QMYalbMERpysCPLNASiQrKU0nrVzvD4uhKrIRADWipA"/>
+    <meta http-equiv="content-language" content="en">
+    <meta name="author" content="Archware">
+    <meta name="generator" content="Archware">
+    <meta name="description" content="@yield('description')">
+    <meta property="article:published_time" content="@yield('published_time')">
+    <meta property="article:section" content="@yield('article:section')">
+    <link rel="icon" href="/storage/sounds/mirror2.png" type="image/x-icon">
+    <meta property="article:tag" content="@yield('title')" />
+    <meta property="og:description" content="@yield('description')"/>
+    <meta property="og:title" content="@yield('title')"/>
+    <meta property="og:url" content="@yield('url')"/>
+    <meta property="twitter:card" content="summary_large_image"/>
+    <meta property="twitter:image:alt" content="@yield('title')"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:locale" content="en_GB"/>
+    <meta property="og:image" content="@yield('image')"/>
+    <meta property="og:site_name" content="Archware"/>
+
+
+    <title>@yield('title', 'Archware')</title>
+
+    <!-- Fonts -->
+    <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('customCSS/archware_style.css') }}" rel="stylesheet">
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('customJS/jquery.js') }}"></script>
+
+</head>
+<body>
+  <div id="app">
+    @include('inc.navbar')
+
+    <main class="py-1">
+      <div class="container">
+        @yield('content')
+      </div>
+    </main>
+
+
+    @include('inc.footer')
+  </div>
+
+  @yield('scripts')
+</body>
+</html>
+
