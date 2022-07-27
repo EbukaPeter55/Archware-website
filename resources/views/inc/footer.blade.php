1cdcbebaeb350dbe2f35420a090625a32149e434
["JOY", "MirrorVlogger"] <fajendagbajoy@gmail.com>
2022-07-27 09:45:38 +0100

    new footer design

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,23 +1,25 @@
 <section>
     <div class="my-5">
 
-        <footer class="text-center text-white" style="background-image: linear-gradient(to right, #06377e , #06546d);">
+        <footer class="text-center text-white" style="background-color: #1f3f95;">
 
-            <div class="container p-4">
-                <h3 class="fw-bold">Newsletter</h3>
-                <p class="pt-2 archware-text-dull">
-                    Be the first to know about discounts, offers and events weekly <br> in your mailbox. Unsubscribe
-                    whenever you like one click
-                </p>
+            <div class="container p-5">
+                <div class="m-4">
+                    <h3 class="fw-bold">Newsletter</h3>
+                    <p class="pt-2">
+                        Be the first to know about discounts, offers and events weekly <br> in your mailbox. Unsubscribe
+                        whenever you like one click
+                    </p>
+                </div>
 
-                <section>
+                <section class="m-3">
                     <form>
                         <div class="row d-flex justify-content-center">
 
                             <div class="TopHeader">
                                 <div class="InputBox">
                                     <i class="fab fa-youtube inputBoxItem"></i>
-                                    <input type="search" placeholder="Enter your email" id="SearchInput" style="background-image: linear-gradient(to right, #06377e , #06546d); color:white;">
+                                    <input type="search" placeholder="Enter your email" id="SearchInput" style="background-color: #1f3f95; color:white;">
                                     <div class="col-auto">
                                         <button type="submit" class="btn btn-outline-light mb-4 footer_submit_button">
                                             Submit
@@ -40,20 +42,20 @@
                 <div class="row">
 
                     <div class="col-md-6 " style="text-align: initial;">
-                        <h5 style="padding-bottom: 20px;">
+                        <h5 class="pb-3">
                             <a href="#" id="LoginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <img src="{{ asset('customImages/archware.png') }}" alt="archwareLog default profile pix" class="user__photo" style="height: 3.5rem; width: 3.8rem; margin-right: 0.2rem">
                             </a>
                         </h5>
 
-                        <ul class="list-unstyled mb-0 archware-text-dull">
-                            <li>
+                        <ul class="list-unstyled mb-0">
+                            <li class="pb-3">
                                 We have an unrivaled reputation for quality in the services we give <br>
                                 due to our integrity and expertise in building systems to manage <br>
                                 people.
                             </li>
                         </ul>
-                        <section class="mb-4">
+                        <section class="pb-3">
                             <a class="m-1 btn btn-outline-light" href="#!" role="button"><i class="fab fa-facebook"></i></a>
 
                             <a class="m-1 btn btn-outline-light" href="#!" role="button"><i class="fab fa-youtube"></i></a>
@@ -66,22 +68,22 @@
 
 
                     <div class="col-md-2" style="text-align: initial">
-                        <h5 class="fw-bold">Pages</h5>
+                        <h5 class="pb-3 fw-bold">Pages</h5>
 
-                        <ul class="list-unstyled mb-0 fw-lighter archware-text-dull">
-                            <li>
+                        <ul class="list-unstyled mb-0 fw-lighter">
+                            <li class="pb-2">
                                 <a href="#!" class="text-white text-decoration-none">Home</a>
                             </li>
-                            <li>
+                            <li class="pb-2">
                                 <a href="#!" class="text-white text-decoration-none">About Us</a>
                             </li>
-                            <li>
+                            <li class="pb-2">
                                 <a href="#!" class="text-white text-decoration-none">Services</a>
                             </li>
-                            <li>
+                            <li class="pb-2">
                                 <a href="#!" class="text-white text-decoration-none">Subsidiaries</a>
                             </li>
-                            <li>
+                            <li class="pb-2">
                                 <a href="#!" class="text-white text-decoration-none">Franchise</a>
                             </li>
                         </ul>
@@ -89,26 +91,26 @@
 
 
                     <div class="col-md-2" style="text-align: initial">
-                        <h5 class="fw-bold">Subsidiaries</h5>
+                        <h5 class="pb-3 fw-bold">Subsidiaries</h5>
 
-                        <ul class="list-unstyled mb-0 fw-lighter archware-text-dull">
-                            <li>Youhi Media</li>
-                            <li>Formost Eyeclinic</li>
-                            <li>Archware Foodbank</li>
-                            <li>Archware Consulting</li>
-                            <li>Farm Management</li>
+                        <ul class="list-unstyled mb-0 fw-lighter">
+                            <li class="pb-2">Youhi Media</li>
+                            <li class="pb-2">Formost Eyeclinic</li>
+                            <li class="pb-2">Archware Foodbank</li>
+                            <li class="pb-2">Archware Consulting</li>
+                            <li class="pb-2">Farm Management</li>
                         </ul>
                     </div>
 
 
                     <div class="col-md-2" style="text-align: initial">
-                        <h5 class="fw-bold">Location</h5>
+                        <h5 class="pb-3 fw-bold">Location</h5>
 
-                        <ul class="list-unstyled mb-0 fw-lighter archware-text-dull">
-                            <li>
+                        <ul class="list-unstyled mb-0 fw-lighter">
+                            <li class="pb-2">
                                 3, Ayanboye Street,<br>Anthony Village, Lagos
                             </li>
-                            <li>
+                            <li class="pb-2">
                                 Ph: +234709154822
                             </li>
                         </ul>
@@ -118,7 +120,7 @@
 
             </section>
 
-            <div class="text-center p-3 archware-text-dull">
+            <div class="text-center pb-5 mt-5">
                 © Archware Technologies International Limited 2022. All Rights Reserved.
             </div>
 
