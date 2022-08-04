d2122f6658c6957391441f2963a6b428e7d96f4d
Ebuka Peter <peterdbrainy5@gmail.com>
2022-08-04 09:54:53 +0100

    Add a dropdown for subsidiaries in the Navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0" style="padding-top: 1.2rem">
     <div class="container col-md-11">
-        <a class="navbar-brand" href="#!">
+        <a class="navbar-brand" href="#!" style="margin-left: 9%;">
             <img src="{{ asset('customImages/Logo.svg') }}">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -8,8 +8,8 @@
             <i class="fas fa-bars"></i>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto" style="margin-left: 8em;">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-top: .5rem">
+            <ul class="navbar-nav me-auto" style="margin-left: 8%;">
                 <li class="nav-item px-3">
                     <a class="nav-link archware-nav-title" href="/">Home</a>
                 </li>
@@ -19,14 +19,23 @@
                 <li class="nav-item px-3">
                     <a class="nav-link archware-nav-title" href="/services">Services</a>
                 </li>
-                <li class="nav-item px-3">
-                    <a class="nav-link archware-nav-title" href="/subsidiaries">Subsidiaries</a>
+                <div class="dropdown" style="margin-top: .1rem">
+                <li class="btn btn-white archware-nav-title dropdown-toggle" data-bs-toggle="dropdown">
+                    Subsidiaries
                 </li>
+                <ul class="dropdown-menu dropdown-style">
+                    <li><a class="dropdown-item" href="#">Youhi Media</a></li>
+                    <li><a class="dropdown-item" href="#">Formost Eyeclinic</a></li>
+                    <li><a class="dropdown-item" href="#">Archware Foodbank</a></li>
+                    <li><a class="dropdown-item" href="#">Archware Consulting</a></li>
+                    <li><a class="dropdown-item" href="#">Farm Management</a></li>
+                </ul>
+                </div>
                 <li class="nav-item px-3">
                     <a class="nav-link archware-nav-title" href="#!">Franchise</a>
                 </li>
             </ul>
-            <ul class="navbar-nav d-flex flex-row mb-3" style="margin-right: 4%">
+            <ul class="navbar-nav d-flex flex-row mb-3" style="margin-right: 9%; margin-top: .5rem">
                 <li class="nav-item me-3 me-lg-0">
                     <button class="btnnav">Contact Us</button>
                 </li>
