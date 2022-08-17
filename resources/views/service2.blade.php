74050ce9444acec165ddccf079de7ab8724677b9
["JOY", "MirrorVlogger"] <fajendagbajoy@gmail.com>
2022-08-17 12:34:12 +0100

    service 2 update

--- resources/views/service2.blade.php
+++ resources/views/service2.blade.php
@@ -14,7 +14,8 @@
         <section style="margin-left:20%;">
             <div class="row justify-items-center">
                 <div class="col-md-6 px-4">
-                    <h2 style="font-weight: 700; font-size: 50px; line-height: 58px; color: #040856;">Website Design<br>and development
+                    <h2 style="font-weight: 700; font-size: 50px; line-height: 58px; color: #040856;">Website Design<br>and
+                        development
                     </h2>
                     <p class="px-2 py-4"
                         style="font-weight: 400;  font-size: 16px;line-height: 172%;color: rgba(4, 8, 86, 0.7);">
@@ -112,8 +113,118 @@
         </div>
     </div>
 
-    <div style="background: #f7f7f7" class="p-1">
+    <div class="my-4">
+        <div style="background: #1F3F95; height: 246px;"
+            class="mt-5 d-flex justify-content-center align-items-center flex-wrap">
+            <div style="margin-top: -70px;">
+                <img src="/customImages/js.png" class="px-5">
+                <img src="/customImages/php.png" class="px-5">
+                <img src="/customImages/html5.png" class="px-5">
+                <img src="/customImages/shopify.png" class="px-5">
+                <img src="/customImages/dotnet.png" class="px-5">
+                <img src="/customImages/wordpress.png" class="px-5">
+                <img src="/customImages/a logo.png" class="px-5">
+                <img src="/customImages/mysql.png" class="px-5">
+            </div>
+        </div>
+
+        <div style="background-image: linear-gradient(to right, #7CB142, #63A148); height: 137px;
+            margin-right:310px; margin-top: -50px;"
+            class="d-flex justify-content-center align-items-center flex-wrap">
+
+            <span
+                style="margin-left: auto; margin-right: 0; font-weight: 700; font-size: 30px; line-height: 36.31px; color: #ffffff;">Ready
+                to start your Web Development project?</span>
+
+            <button type="submit" class=""
+                style="width: 160px;height: 137px;background: #040856;border: none;
+            font-weight: 700;font-size: 39px;line-height: 38px;
+            color: #ffffff; margin-left: auto; margin-right: 0;">LET’S
+                TALK</button>
+        </div>
     </div>
+
+    <div class="container mt-5">
+        <div class="p-5 row">
+
+            <div class="col-md-4">
+                <div class="card" style="border-radius: 20px; box-shadow: 0 5px 10px #e6e6e6; height: 37em;">
+                    <img src="/customImages/Rectangle 661.jpg" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center align-items-center flex-wrap p-4">
+                        <span class="pt-1" style="font-weight: 700; font-size:26px; line-height: 40.3px; color: #0D2158;">
+                            Web App Develppment
+                        </span>
+                        <p class="card-text archware-text-dull"
+                            style="color: #0d2158; font-size: 15px; text-align: center;">
+                            Beyond design, we offer a full stack development approach to developing a massive range of
+                            solutions,
+                            most of which are customized to your business needs. Our team employs a holistic approach to
+                            ensure your
+                            website is visually appealing and easy to navigate.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card" style="border-radius: 20px; box-shadow: 0 5px 10px #e6e6e6; height: 37em;">
+                    <img src="/customImages/Rectangle Ecommerce.jpg" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center align-items-center flex-wrap p-4">
+                        <span class="pt-1" style="font-weight: 700; font-size:26px; line-height: 40.3px; color: #0D2158;">
+                            Ecommerce
+                        </span>
+                        <p class="card-text archware-text-dull"
+                            style="color: #0d2158; font-size: 15px; text-align: center;">
+                            Get your products in front of your customers with our e-commerce website design and development
+                            service. We offer a range of
+                            e-commerce website development packages to help your company succeed.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card" style="border-radius: 20px; box-shadow: 0 5px 10px #e6e6e6; height: 37em;">
+                    <img src="/customImages/Rectangle Ecommerce.jpg" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center align-items-center flex-wrap p-4">
+                        <span class="pt-1"
+                            style="font-weight: 700; font-size:26px; line-height: 40.3px; color: #0D2158;">
+                            B2B & B2C Portals
+                        </span>
+                        <p class="card-text archware-text-dull"
+                            style="color: #0d2158; font-size: 15px; text-align: center;">
+                            We develop B2B and B2C portals
+                            and connect your manufacturers, wholesalers, distributors, and end-users. With these highly
+                            scalable portal solutions, we will aid you in strengthening your customer relationships and
+                            streamlining operational efficiency.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <section class="container-fluid team-section">
+        <div class="d-flex flex-row flex-wrap justify-items-center team-wrapper" style="background: #1F3F95">
+            <img class="img-fluid p-5" style="margin-bottom:0px"
+                src="/customImages/Rectangle corporate.jpg">
+            <div class="team-section-right">
+                <h2>Corporate Website Solutions</h2>
+                <p>
+                    When using any web development technology, including wooCommerce, WordPress, Shopify, Angular, ReactJS,
+                    Laravel, Lumen, PHP, MySQL, JAVA, .Net, JavaScript, Node.js, and HTML, our team of skilled web
+                    developers can construct a great website for your company that is sure to draw in new clients.
+                </p>
+                <p>
+                    Our team of web designers increases your company's chances of success while also giving it an excellent
+                    appearance. In order to provide a better user experience and increase discussion rates, our
+                    professionals are aware of the search engine optimization (SEO) web development requirements.
+                </p>
+            </div>
+        </div>
+    </section>
 
 
 @endsection
