20accdb0f502c54eaada3f131b1676478f0de716
["JOY", "MirrorVlogger"] <fajendagbajoy@gmail.com>
2022-08-18 15:34:24 +0100

    web maintenance with pms link

---
+++ resources/views/services/web-maintenance.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.app')
+
+@section('description',
+    'Archware - Our agile approach to problem-solving and strategic planning helps you achieve
+    better results for your business.')
+@section('title', 'Archware - Excellent Tech-based Solutions For Your Business')
+@section('article:section', 'Home')
+@section('url', 'www.archwareltd.com')
+@section('image'){{ asset('customImages/Logo.svg') }}@stop
+
+@section('content')
+
+    <section style=" background-image: url(../customImages/Group68.png)" ; class="mt-5">
+        <div class="container justify-content-center">
+            <div class="row">
+                <div class="col-md-5 mt-5 pt-3">
+                    <h2 style="font-weight: 700; font-size: 50px; line-height: 58px; color: #040856;">
+                        Web Maintenance
+                    </h2>
+                    <p class="pt-3"
+                        style="font-style: normal;font-weight: 400;font-size: 18px;line-height: 155%;color: rgba(4, 8, 86, 0.7);">
+                        Optimal customer experience on your website without <br>
+                        downtime.
+                    </p>
+                </div>
+                <div class="col-md-7 px-2" style="">
+                    <img class="img-fluid" src="{{ asset('/customImages/web maintenance right.png') }}">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="container mt-5">
+        <div class="row">
+            <div class="col-md-5" class="w-50">
+                <img style="width: 500px; " class="img-fluid" src="/customImages/web maintenance left.png">
+            </div>
+            <div class="col-md-6 px-4">
+                <p class="px-2 py-4"
+                    style="font-weight: 400;  font-size: 16px;line-height: 172%;color: rgba(4, 8, 86, 0.7);">
+                    Having a responsive website is a great deal of work that needs to be done to successfully scale your
+                    business. The truth is, a website is never really complete due to the ever-evolving demands and changes
+                    happening in the digital world. This is why it is important to periodically make plans to update the
+                    website with fresh content to engage your growing audience. Without frequent maintenance, your website
+                    will start looking stale and may develop errors due to technical environment changes.
+                    We add value to your business by making sure you’re not missing out on traffic and revenue growth as a
+                    result of a sluggish website or website downtime. Our value in performance extends to our web
+                    maintenance plan with our web maintenance team ensuring that your website is up to date regarding
+                    design, content, regular backups, and protection against malicious login attempts, spam, bugs, and
+                    malware.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="my-3 py-3">
+        <div class="container justify-content-center">
+            <div class="row">
+                <a class="py-3" style="text-align:center;">
+                    <button
+                        style="background: #1F3F95;border-radius: 4px;border:none; width:521px; height: 56px; color:#FFFFFF">
+                        Let’s Talk About Your E-commerce Project
+                    </button>
+                </a>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="container mt-5">
+        <div class="p-5 row">
+
+            <div class="col-md-4 p-3">
+                <div class="card" style="border-radius: 20px; box-shadow: 0 5px 10px #e6e6e6; height: 37em;">
+                    <img src="/customImages/Broaden Your Brand.jpg" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center align-items-center flex-wrap p-4">
+                        <div class="pt-1" style="font-weight: 700; font-size:26px; line-height: 40.3px; color: #0D2158;">
+                            Broaden Your Brand
+                        </div>
+                        <p class="card-text archware-text-dull"
+                            style="color: #0d2158; font-size: 15px; text-align: center; margin-bottom: auto;">
+                            Ecommerce is an ideal way you can take your brand from a traditional store to an innovative,
+                            well-loved brand, and maintain exponential growth.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 p-3">
+                <div class="card" style="border-radius: 20px; box-shadow: 0 5px 10px #e6e6e6; height: 37em;">
+                    <img src="/customImages/More Convenience.png" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center align-items-center flex-wrap p-4">
+                        <div class="pt-1" style="font-weight: 700; font-size:26px; line-height: 40.3px; color: #0D2158;">
+                            More Convenience
+                        </div>
+                        <p class="card-text archware-text-dull"
+                            style="color: #0d2158; font-size: 15px; text-align: center; margin-bottom: auto;">
+                            An online store is available all day, every day meaning your customers can visit your store at
+                            all times, no matter what their schedule might be. This means you can appeal to a wider range of
+                            customers all looking for a convenient and flexible experience.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 p-3">
+                <div class="card" style="border-radius: 20px; box-shadow: 0 5px 10px #e6e6e6; height: 37em;">
+                    <img src="/customImages/Marketing Opportunities.png" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center align-items-center flex-wrap p-4">
+                        <div class="pt-1" style="font-weight: 700; font-size:26px; line-height: 40.3px; color: #0D2158;">
+                            Marketing Opportunities
+                        </div>
+                        <p class="card-text archware-text-dull"
+                            style="color: #0d2158; font-size: 15px; text-align: center; margin-bottom: auto;">
+                            Your website is one of the best marketing tools your business has. A great online store design
+                            not only help customers trust your brand but also helps you stand out from the competition.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <section class="container-fluid team-section">
+        <div class="d-flex flex-row flex-wrap justify-items-center team-wrapper"
+            style="background: #1F3F95; border-radius: 20px 20px;">
+            <img class="img-fluid p-5" style="margin-bottom: -48px; margin-top: auto; border-radius: 70px 70px 30px 50px;"
+                src="/customImages/Our Experience.png">
+            <div class="team-section-right">
+                <h2>Our Experience</h2>
+                <p>
+                    Our team of e-commerce web design experts will help you <br>
+                    every step of the way to bring your vision to life with precise <br>
+                    planning and execution to present an attractive display of <br>
+                    your products to your customers and improve your sales.<br>
+                    Our goal is to create an e-commerce website that is fast, and <br>
+                    functional to keep your customers returning and converting <br>
+                    from their first visit. All you have to do is let us know your <br>
+                    goals, and we'll do the research for you.<br>
+                    Our job isn't finished once your site is launched. We will also <br>
+                    maintain and optimise your website, keeping it up to date <br>
+                    and functioning so that you can get the most out of your new <br>
+                    e-commerce website development.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="my-5 py-5">
+        <div class="container justify-content-center">
+            <div class="row">
+                <p style="font-weight: 600;font-size: 30px;line-height: 72px;color: #0D2158; text-align:center">
+                    Let's create an e-commerce store as great as your products
+                </p>
+                <a class="py-3" style="text-align:center;">
+                    <button
+                        style="background: #1F3F95;border-radius: 4px;border:none; width:312px; height: 56px; color:#FFFFFF">
+                        Get In Touch For a Quote
+                    </button>
+                </a>
+            </div>
+        </div>
+    </section>
+
+
+@endsection
