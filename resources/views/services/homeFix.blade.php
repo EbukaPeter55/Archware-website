31462389df9a645a77f10f0a4e21e3bc1652e2ff
Ebuka Peter <peterdbrainy5@gmail.com>
2022-08-19 16:14:02 +0100

    Create a new file and route homepageFix

---
+++ resources/views/services/homeFix.blade.php
@@ -0,0 +1,33 @@
+<!-- @extends('layouts.app')
+
+@section('description',
+    'Archware - Our agile approach to problem-solving and strategic planning helps you achieve
+    better results for your business.')
+@section('title', 'Archware - Excellent Tech-based Solutions For Your Business')
+@section('article:section', 'Home')
+@section('url', 'www.archwareltd.com')
+@section('image'){{ asset('customImages/Logo.svg') }}@stop
+
+@section('content') -->
+
+<h1>Home page</h1>
+<h1>Home page</h1><h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
+<h1>Home page</h1>
