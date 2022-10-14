92de76b46bb0c7e963d1356c9c15f2cba0826200
Ebuka Peter <peterdbrainy5@gmail.com>
2022-10-14 12:36:14 +0100

    Update the meta description

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,10 +3,6 @@
  <div class="loader-container">
     <div class="spinner"></div>
   </div>
-@section('description',
-    'Archware - Africa\'s Leading Software Development Company
-Software development company helping businesses and individuals take advantage of the benefits of the global technology landscape.')
-@section('title', 'Archware - Excellent Tech-based Solutions For Your Business')
 @section('article:section', 'Home')
 @section('url', 'www.archwareltd.com')
 @section('image'){{ asset('customImages/Logo.svg') }}@stop
