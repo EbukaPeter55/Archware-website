903e0d653fc6f9a60d20b7484167bc97ec4346dc
joy <joy.fajendagba@archwaretechgroup.com>
2023-01-03 16:41:06 +0100

    recruit update

--- resources/views/mail/recruit/data.blade.php
+++ resources/views/mail/recruit/data.blade.php
@@ -1,12 +1,12 @@
-@component('mail::message')
-# Introduction {{ $data }}
+{{-- @component('mail::message')
+# Introduction {{ $data }} --}}
 
 The body of your message.
 
-@component('mail::button', ['url' => ''])
+{{-- @component('mail::button', ['url' => ''])
 Button Text
 @endcomponent
 
 Thanks,<br>
 {{ config('app.name') }}
-@endcomponent
+@endcomponent --}}
