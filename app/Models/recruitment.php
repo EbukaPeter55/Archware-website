a3dd20caf54bb15c0a2b9d126e5764395a2f2154
joy <joy.fajendagba@archwaretechgroup.com>
2023-01-19 09:52:18 +0100

    recruitment

---
+++ app/Models/recruitment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class recruitment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'post_applied',
+        'nationality',
+        'qualification',
+        'major_in',
+        'school_name',
+        'date_graduated',
+        'saudi_council_question',
+        'sce_number',
+        'sce_validity_date',
+        'certificate_training',
+        'work_experience_yrs',
+        'field_experience_yrs',
+        'qc_inspection_experience',
+        'aramco_qa_qc_approval',
+        'aramcosap_no_or_id',
+        'english_skills',
+        'current_salary',
+        'expected_salary',
+        'id_no',
+        'transferrable_iqama',
+        'driving_license',
+        'dob',
+        'country',
+        'city',
+        'notice_period',
+        'contact_no',
+        'whatsapp_or_imo',
+        'skype_id',
+        'email',
+    ];
+}
